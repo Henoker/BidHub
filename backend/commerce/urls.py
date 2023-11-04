295a05181fc5e3b8b4bef10fd42e65a1880de737
Henoker <hennybany@gmail.com>
2023-11-04 09:23:09 +0300

    feat: serializer for custom user registration

--- backend/commerce/urls.py
+++ backend/commerce/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view()),
     path('api/token/refresh/', TokenRefreshView.as_view()),
     path('api/token/verify/', TokenVerifyView.as_view()),
+    path('api/users/', include('users.urls')),
     
 ]
