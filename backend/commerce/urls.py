630f58ab46c9d937d9e7212de8d79b031108e4eb
Henoker <hennybany@gmail.com>
2023-11-04 08:41:39 +0300

    initial setup:  django, users app postgres db

---
+++ backend/commerce/urls.py
@@ -0,0 +1,12 @@
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
+    path('api/token/verify/', TokenVerifyView.as_view()),
+    
+]
