c6893195666ba39f2f2f2f8e0bc34810f802b3de
Henoker <hennybany@gmail.com>
2024-01-13 21:34:55 +0300

    feat: model user serilizers

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,11 +1,15 @@
-from django.contrib.auth import get_user_model
-from djoser.serializers import UserCreateSerializer
+from rest_framework.serializers import ModelSerializer
+from .models import User
 
-
-User = get_user_model()
-
-
-class CreateUserSerializer(UserCreateSerializer):
-    class Meta(UserCreateSerializer.Meta):
+class UserSerializer(ModelSerializer):
+    class Meta:
         model = User
-        fields = ['id', 'email', 'first_name', 'last_name', 'password']
+        fields = ['id', 'first_name', 'last_name', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+        
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import exceptions
+from .serializers import UserSerializer
 
 class RegisterApiView(APIView):
     def post(self, request):
-        return Response(request.data) 
+        data = request.data
+        if data['password']!= data['password_confirmation']:
+            raise exceptions.ApiException('Passwords do not match')
+        serializer = UserSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
